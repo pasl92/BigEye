2ceafe200b67e1ad2f2e2bb6b52d62a0beb9fabd
pasl92 <pawel.salata@microsoft.wsei.edu.pl>
2022-01-11 20:49:34 +0100

    set layout and details + back button

--- app/src/main/java/com/example/bigeye/LoginActivity.kt
+++ app/src/main/java/com/example/bigeye/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bigeye
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bigeye.databinding.ActivityLoginBinding
+import com.example.bigeye.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            binding.backButton.setOnClickListener{
+                var backActivity: Intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(backActivity)
+            }
     }
 }
--- app/src/main/java/com/example/bigeye/MainActivity.kt
+++ app/src/main/java/com/example/bigeye/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.bigeye
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import com.example.bigeye.databinding.ActivityMainBinding
 
 
