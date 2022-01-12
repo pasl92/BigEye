f3dd09da2af1bdba9e58a1f8a10778278e0d8341
pasl92 <pawel.salata@microsoft.wsei.edu.pl>
2022-01-12 22:27:17 +0100

    add xml to singup page

--- app/src/main/java/com/example/bigeye/SingupActivity.kt
+++ app/src/main/java/com/example/bigeye/SingupActivity.kt
@@ -2,10 +2,15 @@ package com.example.bigeye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bigeye.databinding.ActivityLoginBinding
+import com.example.bigeye.databinding.ActivitySingupBinding
 
 class SingupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySingupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_singup)
+        binding = ActivitySingupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
